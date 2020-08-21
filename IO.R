2a97cd11a79644df576938953eef81df15eeecc9
longrui <18813867725@163.com>
2020-08-21 11:19:41 +0800

    io

---
+++ IO.R
@@ -0,0 +1,14 @@
+mydata <- data.frame(age = numeric(0),gender = character(0),weight = numeric(0))
+edit(mydata)
+
+mydata
+fix(mydata)
+
+
+mydataframe <- read.csv("worker.csv",sep = ",")
+mydataframe
+library(RODBC)
+channel <- odbcConnectExcel2007("worker.xlsx")
+mydata_frame <- sqlFetch(channel,"Sheet1")
+odbcClose(channel)
+mydata_frame
